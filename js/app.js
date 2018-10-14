fc6d9dc9a64af935848dce6b671c30231e5b37aa
adsingh14 <deep.amansingh14@gmail.com>
2018-10-14 23:59:01 +0530

    Added character and it's movements

--- js/app.js
+++ js/app.js
@@ -1,46 +1,98 @@
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = "images/enemy-bug.png";
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
+/**
+ * @description player class named 'Hero'
+ * @param {number} this.step - Vertical Steps - COLUMNS
+ * @param {number} this.jump - Horizontal Steps - ROWS
+ * @param {number} this.startX - Centralize Horizontal Step per block
+ * @param {number} this.startY - Centralize Vertical Step per block
+ */
 // This class requires an update(), render() and
 // a handleInput() method.
+class Hero {
+  constructor() {
+    this.sprite = "images/char-boy.png";
+    this.step = 101;
+    this.jump = 83;
+    this.startX = this.step * 2;
+    this.startY = this.jump * 5 - 40;
+    this.x = this.startX;
+    this.y = this.startY;
+  }
 
+  // renders character's image
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+
+  /**
+   * @param {string} input - variable for direction
+   * @description Binding controls with Hero where
+   * @description 'x' denotes horizontal path & 'y' denotes vertical path
+   * @description use of `if-condition` to keep our Hero in the playground
+   */
+
+  handleInput(input) {
+    switch (input) {
+      case "left":
+        if (this.x > 0) {
+          this.x -= this.step;
+        }
+        break;
+      case "up":
+        if (this.y > 0) {
+          this.y -= this.jump;
+        }
+        break;
+      case "right":
+        if (this.x < this.jump * 4) {
+          this.x += this.step;
+        }
+        break;
+      case "down":
+        if (this.y < this.jump * 4) {
+          this.y += this.jump;
+        }
+        break;
+    }
+  }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+const player = new Hero();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+document.addEventListener("keyup", function(e) {
+  var allowedKeys = {
+    37: "left",
+    38: "up",
+    39: "right",
+    40: "down"
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
